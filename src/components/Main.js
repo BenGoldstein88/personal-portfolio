359d9c5320a7e0f33af8746644bc0bcec4043bad
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-12-04 15:06:22 -0800

    initial commit for soft-deploy

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import '../assets/@blueprintjs/core/dist/blueprint.css';
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <div>{this.props.children}</div>
+    );
+  }
+}
